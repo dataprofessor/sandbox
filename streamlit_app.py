1887958bb2395f57c612047528314087b13a7868
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-19 23:50:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ with cols[2]:
 
 genre = st.radio(
     "What's your favorite movie genre",
-    [":rainbow[Comedy]", "[Drama](http://www.google.com)", "Documentary :movie_camera:"],
-    captions = ["Laugh out loud.", "Get the popcorn.", "Never stop learning."])
+    [":rainbow[Comedy]", "Drama", "Documentary :movie_camera:"],
+    captions = ["Laugh out loud.", "Get the [popcorn](http://www.google.com).", "Never stop learning."])
 
 
